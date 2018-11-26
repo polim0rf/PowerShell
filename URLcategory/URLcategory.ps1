68d596b859891931f4cee5b8979c207664a057ef
polim0rf <39036146+polim0rf@users.noreply.github.com>
2018-11-26 15:08:12 +0100

    Update URLcategory.ps1

--- URLcategory/URLcategory.ps1
+++ URLcategory/URLcategory.ps1
@@ -1,41 +1,30 @@
 <#
 .SYNOPSIS
-
   Get and submit BlueCoat category information for URL's
-
 .DESCRIPTION
-
   This script retrieves category information for a list of URLs, and submit to Sitereview the ones 
   that are still Unrated.
-
 .PARAMETER
-
     -url: URL to categorize
     -cat: Integer number indicating possible category
     -comment: Required comment
     -list: List of URLs in TXT file
     
 .NOTES
-
   Version:        1.0.0
   Author:         polim0rf
   Creation Date:  23.11.2018
   Purpose/Change: Initial script development
-
   DISCLAIMER: Please be aware that querying SiteReview.com via scripts 
   is against their ToS. This script POC is only for educational purposes.
   
 .EXAMPLE
-
     Query single URL:
     URLcategory -cat 18 -comment Phishing -url https://mmmmkool.godaddysites.com 
-
     Query list of URL from TXT file:
     URLcategory -cat 18 -comment Phishing -list ./list.txt
-
     Type below command to see current list of categories:
     Show-Categories
-
 #>
 
 
@@ -55,8 +44,9 @@ Function URLcategory {
     ### LINES BELOW SHOULD BE MODIFIED AT FIRST SCRIPT EXECUTION
     $email1 = ""
     $email2 = ""
-    $sendEmail = "false"
+    $sendEmail = "true"
     $referrer = "bluecoatsg" #Default > Blue Coat SG
+    $cat2 = ""
 
     #Referrer Keyword list:
     #Blue Coat CachFlow Appliance   = cacheflow
@@ -96,6 +86,8 @@ Function URLcategory {
 filter Categorize {
 
     $Payload = ""
+    $myWebSession = ""
+    $categorized = "false"
     $Payload = @{url = $url ; captcha = ""; }
     $json = $Payload | ConvertTo-Json
 
@@ -104,11 +96,12 @@ filter Categorize {
     Write-Host "`r"
 
     $po = Invoke-RestMethod -Method 'POST' -uri "http://sitereview.bluecoat.com/resource/lookup" -H $Headers -Body $json -SessionVariable myWebSession
+    $po
 
     If ($po.unrated -eq 'True') {
  
         Write-host -Fore DarkYellow '    This url was not previously categorized. Starting submission...'
-        $Payload = @{comments = $comment ; email1 = $email1; email2 = $email2; cat1 = $cat; referrer = $referrer; sendEmail = $sendEmail; trackid = ""}
+        $Payload = @{comments = $comment ; email1 = $email1; email2 = $email2; cat1 = $cat; cat2 = $cat2; referrer = $referrer; sendEmail = $sendEmail; trackid = ""}
         $json = $Payload | ConvertTo-Json
 
         $response = Invoke-WebRequest -Method 'POST' -uri "http://sitereview.bluecoat.com/resource/submitCategorization" -H $Headers -Body $json -WebSession $myWebSession
@@ -124,43 +117,71 @@ filter Categorize {
         
         $item = New-Object PSObject
         Write-Host -Fore Cyan 'Url already categorized as:'
-        Write-Host '*******************************'
-        $po.securityCategoryIds | foreach {
+        Write-Host '*****************************'
+        $po.categorization.num | foreach {
             $secCat = $_
             $out = $categ | Where-Object {$_.'num' -like $secCat} | Select-Object 'name'
             Write-Host -Fore DarkCyan '   '$out.name
             $item | Add-Member -type NoteProperty -Name $secCat -Value $out.name
             
         }
-        Write-Host '*******************************'
+        Write-Host '*****************************'
 
         $po | Add-Member -type NoteProperty -Name 'TranslatedCategory' -Value $item
         $Export = $po | ParseData
         $Export | Export-Csv -Path .\Output\Categorized.csv -NoType -Encoding UTF8 -Append -Force
-    }
 
+        # If category not malicious, re-submit
+        $po.categorization.num | foreach {
+            If (($_ -eq '43') -or ($_ -eq '102') -or ($_ -eq '44') -or ($_ -eq '92') -or ($_ -eq '18')) {
+                $categorized = "true"
+            }
+            Else {
+                $categorized = "false"
+            }
+        }
+        If ($categorized -eq "false") {
+
+            Write-Host -Fore Cyan 'Sending re-categorization...'
+        
+            $Payload = @{comments = $comment ; email1 = $email1; email2 = $email2; cat1 = $cat; cat2 = $cat2; referrer = $referrer; sendEmail = $sendEmail; trackid = ""}
+            $json = $Payload | ConvertTo-Json
+
+            $response = Invoke-WebRequest -Method 'POST' -uri "http://sitereview.bluecoat.com/resource/submitCategorization" -H $Headers -Body $json -WebSession $myWebSession
+            $jsonResponse = $response.Content | ConvertFrom-Json
+            Write-Host -Fore DarkCyan '   '$jsonResponse.message
+
+            $response | Add-Member -type NoteProperty -Name 'URL' -Value $url
+            $Export = $response | ParseData
+                
+            $Export | Export-Csv -Path .\Output\Pending_Category.csv -NoType -Encoding UTF8 -Append -Force
+            Write-Host '*****************************'
+            
+             
+        }
+    }
 }
 
 filter ParseData {
 
-     #Parse JSON response to adapt it for an export to CSV
-     $Tablevalues = @()
+    #Parse JSON response to adapt it for an export to CSV
+    $Tablevalues = @()
 
-     $namelist = $_ | Get-Member -MemberType *property
-     $Tablevalues += $namelist.Name #Get first column values of the hashtable (Name)
-     $subcount = 0
-     [hashtable]$TableProperty = @{}
-     For ($i=0;$i -lt $Tablevalues.count;$i++) {     
+    $namelist = $_ | Get-Member -MemberType *property
+    $Tablevalues += $namelist.Name #Get first column values of the hashtable (Name)
+    $subcount = 0
+    [hashtable]$TableProperty = @{}
+    For ($i = 0; $i -lt $Tablevalues.count; $i++) {     
   
         $stringvalue = ($_.($Tablevalues[$subcount]) | Out-String).Trim()
-        $val = $stringvalue.replace("`n","") #Remove newline character (for CSV export)
-        $val = $val -replace(" +"," ") #Remove excess of whitespaces
+        $val = $stringvalue.replace("`n", "") #Remove newline character (for CSV export)
+        $val = $val -replace (" +", " ") #Remove excess of whitespaces
         $TableProperty.Add( $Tablevalues[$subcount], $val )
                 
         $subcount ++
-     }
-     $Table = New-Object -TypeName psobject -Property $TableProperty
-     Return $Table 
+    }
+    $Table = New-Object -TypeName psobject -Property $TableProperty
+    Return $Table 
 
 }
 
